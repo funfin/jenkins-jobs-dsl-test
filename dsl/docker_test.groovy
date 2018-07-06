a33a661ec201286c2a16a215d89338776c26701d
Piotr Karpik <pkarpik@ingdlabiznesu.pl>
2018-07-06 12:18:01 +0200

    add job testing docker

---
+++ dsl/docker_test.groovy
@@ -0,0 +1,18 @@
+def jobName="docker test"
+job(jobName) {
+    steps {
+        shell('''
+            set +x
+            docker version || KO=1
+
+            if [ "$KO" -eq "1" ]; then
+                echo -e "\\n\\ndocker is not available, see https://goo.gl/RpkBZz\\n\\n"
+                exit 1
+            else
+                echo -e"\\n\\ndocker is available\\n\\n"
+                docker images
+            fi
+        '''.stripIndent())
+    }
+    queue(jobName)
+}
